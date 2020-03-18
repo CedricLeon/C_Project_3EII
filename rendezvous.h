eba94318c73cd8aef0e3ac17df92700be76759c3
Magois Angeline <angeline.magois@insa-rennes.fr>
2020-03-18 23:13:42 +0100

    Structure RendezVous et prototypes fonctions

---
+++ rendezvous.h
@@ -0,0 +1,23 @@
+#ifndef RENDEZVOUS_H
+#define RENDEZVOUS_H
+
+#include "date.h"
+#include "medecin.h"
+#include "patient.h"
+
+typedef struct{
+    Date date;
+    int heure;      // Comment on stocke ça ? 16h30 <=> 16.5 ? ou Alors un attribut heure et un attribut minute ?
+    int minute;
+    int duree;  //Tout les rendez-vous ne durent pas aussi longtemps
+    char * lieu; //Numéro batiment / numero salle ?
+    Patient * patient;
+    Medecin * medecin;
+    char * motif;
+}RendezVous;
+
+RendezVous * CreerRendezVous(int an, int mois, int jour, int heure, int duree, char * lieu, Patient * patient, Medecin * medecin, char * motif);
+int AnnulerRendezVous(RendezVous * rdv);
+int DeplacerRendezVous(RendezVous * rdv, int n_an, int n_jour, int n_heure, int n_duree);
+
+#endif
