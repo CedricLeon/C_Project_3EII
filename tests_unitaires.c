12f4379695aa6ceaac17e901f84824b8135d0607
Cedric Leonard <cedric.leonard@insa-rennes.fr>
2020-03-20 16:56:51 +0100

    Creation de tests_unitaires.c et .h pour regarder plus tard avec CMocka et le .gitlab-ci.yml

--- tests_unitaires.c
+++ tests_unitaires.c
@@ -1,4 +1,7 @@
-//
-// Created by cleonard on 20/03/2020.
-//
+#include "tests_unitaires.h"
 
+/*Tout les tests unitaires a la main*/
+/* Cf Cmocka*/
+/*Minis tests tous lancés à la fin dans un espèce de main*/
+
+/*Compiler le tests_unitaires de Cmocka et vérifier que le bon résultat est attendu*/
