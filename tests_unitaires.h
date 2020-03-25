3e778668791546f017eb647eb86c890b2f57c294
Cedric Leonard <cedric.leonard@insa-rennes.fr>
2020-03-25 17:24:43 +0100

    Debut decriture des tests pour patient

--- tests_unitaires.h
+++ tests_unitaires.h
@@ -10,7 +10,7 @@
 #include "dossier_medical.h"
 #include "rendezvous.h"
 
-/*Librairies nécessaire à CMocka*/
+/*Librairies nécessaire à CMocka*/          //pas sûr d'en avoir besoin ici
 #include <stdarg.h>
 #include <stddef.h>
 #include <setjmp.h>
@@ -18,9 +18,28 @@
 
 /*Librairies standart de C*/
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-/*La on va faire du cmocka et on lancera le tout depuis le main.c*/
+/* Tests de la structure Patient */
+static void testPatient_creerPatient(void ** state);
+static void testPatient_deletePatient(void ** state);
+//static void testPatient_inscriptionPatient(void ** state);
 
+static void testPatient_setNomPatient(void ** state);
+static void testPatient_setPrenomPatient(void ** state);
+static void testPatient_setDateNaissancePatient(void ** state);
+static void testPatient_setAdresseMailPatient(void ** state);
+static void testPatient_setNumeroTelephonePatient(void ** state);
+
+static void testPatient_InitMedecinConsultesPatient(void ** state);
+static void testPatient_FreeMedecinsConsultesPatient(void ** state);
+
+static void testPatient_AddMedecinPatient_handlesMedecinAdded(void ** state);
+static void testPatient_AddMedecinPatient_handlesNbMaMedecinAtteient(void ** state);
+static void testPatient_DeleteMedecinPatient_handlesMedecinsEnleve(void ** state);
+static void testPatient_DeleteMedecinPatient_handlesPasDeMedecin(void ** state);
+static void testPatient_DeleteMedecinPatient_handlesMedecinNonPresent(void ** state);
 
 
 #endif //C_PROJECT_TESTS_UNITAIRES_H
