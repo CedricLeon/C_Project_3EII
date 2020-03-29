501beb0874d484ff9aeb1a677799e7ceaf1d2073
Cabioch Elisabeth <elisabeth.cabioch@insa-rennes.fr>
2020-03-29 21:35:52 +0200

    header de callbacks.c

---
+++ callbacks.h
@@ -0,0 +1,15 @@
+/*!
+* \file callbacks.h
+* \brief Provides basic unit testing functions declaration
+*/
+
+#ifndef CALLBACKS_H_INC
+#define CALLBACKS_H_INC
+
+#include <stdlib.h>
+#include <gtk/gtk.h>
+#include "fonctionsGraphiques.h"
+
+void cb_create_entry(GtkWidget *p_widget, gpointer user_data);
+
+#endif
