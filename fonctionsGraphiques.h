8f3cd68078bc96f1e52583e9d4ea2fe636fcdd0d
Cabioch Elisabeth <elisabeth.cabioch@insa-rennes.fr>
2020-03-29 21:37:34 +0200

    header de fonctionsGraphiques.c

---
+++ fonctionsGraphiques.h
@@ -0,0 +1,17 @@
+/*!
+* \file fonctionsGraphiques.h
+* \brief Provides basic unit testing functions declaration
+*/
+
+#ifndef FONCTIONSGRAPHIQUES_H_INC
+#define FONCTIONSGRAPHIQUES_H_INC
+
+#include <stdlib.h>
+#include <gtk/gtk.h>
+#include "callbacks.h"
+
+void boutonRDV(GtkWidget *wid, GtkGrid grid);
+int create_window(int argc, char *argv[]);
+
+#endif
+
