ec6f6d15da3562f91faad499f1c7a6dbacf09f0c
Cedric Leonard <cedric.leonard@insa-rennes.fr>
2020-03-31 17:48:37 +0200

    Correction du bug des tests et implémentation de quelques tests supplémentaires

--- tests_unitaires.c
+++ tests_unitaires.c
@@ -11,9 +11,23 @@
                                     /*Tests des fonctions Patient*/
 /**********************************************************************************************************************/
 
-//static void testPatient_creerPatient(void ** state);
+static void testPatient_creerPatient(void ** state){
+    assert_string_equal(((Patient *) *state)->nom,"NomTest");
+    assert_string_equal(((Patient *) *state)->prenom,"PrenomTest");
+    assert_int_equal(((Patient *) *state)->date_naissance->annee,2020);
+    assert_int_equal(((Patient *) *state)->date_naissance->mois, 03);
+    assert_int_equal(((Patient *) *state)->date_naissance->jour, 29);
+    assert_string_equal(((Patient *) *state)->adresse_mail, "test@adresseMail");
+    assert_string_equal(((Patient *) *state)->numero_telephone,"testNumeroTelephone");
+}
+
 //static void testPatient_deletePatient(void ** state);
-//static void testPatient_inscriptionPatient(void ** state);
+// Je sais pas trop comment faire un test d'un free,
+// j'ai essayé test_free() mais erreur et puis dans tout les cas je fait mon free dans le teardown ...
+// et puis ça existe un free qui plante ?
+
+
+//static void testPatient_inscriptionPatient(void ** state);    Pour plus tard
 
 
 
@@ -40,34 +54,52 @@ static void testPatient_setNumeroTelephonePatient(void ** state){
     assert_string_equal(((Patient *) *state)->numero_telephone,"NewTel");
 }
 
-static void testPatient_InitMedecinConsultesPatient(void ** state);
-static void testPatient_FreeMedecinsConsultesPatient(void ** state);
+static void testPatient_AddMedecinPatient_handlesMedecinAdded(void ** state{
 
-static void testPatient_AddMedecinPatient_handlesMedecinAdded(void ** state);
-static void testPatient_AddMedecinPatient_handlesNbMaMedecinAtteient(void ** state);
-static void testPatient_DeleteMedecinPatient_handlesMedecinsEnleve(void ** state);
-static void testPatient_DeleteMedecinPatient_handlesPasDeMedecin(void ** state);
-static void testPatient_DeleteMedecinPatient_handlesMedecinNonPresent(void ** state);
+}
+static void testPatient_AddMedecinPatient_handlesNbMaMedecinAtteint(void ** state){
 
-//Fonctions de setup et teardown des setteurs de la structure Patient
-static int setup_setteurs_Patient(void ** state){
+}
+static void testPatient_DeleteMedecinPatient_handlesMedecinsEnleve(void ** state){
+
+}
+static void testPatient_DeleteMedecinPatient_handlesPasDeMedecin(void ** state){
+
+}
+static void testPatient_DeleteMedecinPatient_handlesMedecinNonPresent(void ** state){
+
+}
+
+//Fonctions de setup et teardown de la structure Patient
+static int setup_Patient(void ** state){
     Patient * p = CreerPatient("NomTest", "PrenomTest", 2020, 03, 29, "test@adresseMail", "testNumeroTelephone");
     *state = p;
+    return *state == NULL;
 }
-static int teardown_setteurs_Patient(void ** state){
+static int teardown_Patient(void ** state){
     DeletePatient((Patient *) *state);
+    return 0;
 }
 
 int main(void){
     const struct CMUnitTest tests_fonctionsPatient[] = {
+
+            //tests des fonctions de création d'instances
+            cmocka_unit_test(testPatient_creerPatient),
 
             //tests des setteurs de la structure Patient
             cmocka_unit_test(testPatient_setNomPatient),
             cmocka_unit_test(testPatient_setPrenomPatient),
             cmocka_unit_test(testPatient_setDateNaissancePatient),
             cmocka_unit_test(testPatient_setNumeroTelephonePatient),
+
+            //Tests des fonctions d'ajout et de delte de medecins à la liste de mèdecins consultés par le patient
+            cmocka_unit_test(testPatient_AddMedecinPatient_handlesMedecinAdded),
+            cmocka_unit_test(testPatient_AddMedecinPatient_handlesNbMaMedecinAtteint),
+            cmocka_unit_test(testPatient_DeleteMedecinPatient_handlesMedecinsEnleve),
+            cmocka_unit_test(testPatient_DeleteMedecinPatient_handlesPasDeMedecin),
+            cmocka_unit_test(testPatient_DeleteMedecinPatient_handlesMedecinNonPresent),
     };
 
-    return cmocka_run_group_tests(tests_fonctionsPatient, setup_setteurs_Patient, teardown_setteurs_Patient);
-
+    return cmocka_run_group_tests(tests_fonctionsPatient, setup_Patient, teardown_Patient);
 }
