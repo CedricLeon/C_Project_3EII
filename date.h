4927825d305ed02ffb8b8f99126874843fa64aa1
Magois Angeline <angeline.magois@insa-rennes.fr>
2020-04-01 21:21:32 +0200

    Ajout commentaires Doxygen pour AjoutMoisDateCourante

--- date.h
+++ date.h
@@ -1,21 +1,64 @@
-#ifndef DATE_H
-#define DATE_H
+#include "date.h"
 
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+/**
+ * CreerDate : Creer dynamiquement un objet Date
+ * @param annee : l'annee de cette date
+ * @param mois : le mois
+ * @param jour : le jour
+ * @return la date créée
+ */
+Date * CreerDate(int annee, int mois, int jour){
+    Date * d = (Date *) malloc(sizeof(Date));
+    d->annee = annee;
+    d->mois = mois;
+    d->jour = jour;
+    return d;
+}
+/**
+ * FreeDate : Libère la mémoire d'une instance Date
+ * @param d
+ */
+void FreeDate(Date * d){
+    free((void *) d);
+}
+/**
+ * DateCourante : Créer une date correspondant à la date courante
+ * @return la date courante
+ */
+Date * DateCourante(){
+    int day, mois, an;
+    time_t now;
 
-typedef struct{
-    int annee;
-    int mois;
-    int jour;
-}Date;
+  time(&now);
 
-Date * CreerDate(int annee, int mois, int jour);
-void FreeDate(Date * d);
-Date * DateCourante();
-Date * AjoutMoisDateCourante(int nb_mois); /*pour ajouter nbmois mois ï¿½ la date courante (utile pour ordonnance par exemple*/
+  struct tm *local = localtime(&now);
+  day = local->tm_mday;
+  mois = local->tm_mon + 1;
+  an = local->tm_year + 1900;
 
+  Date * d = CreerDate(an,mois,day);
 
-#endif
+  return d;
+}
+
+/**
+ * AjoutMoisDateCourante : Permet d'ajouter nb_mois mois à la date courante (utile notamment pour les ordonnances)
+ * @param nb_mois
+ * @return
+ */
+Date * AjoutMoisDateCourante(int nb_mois){
+    int day, mois, an;
+    time_t now;
+    time(&now);
+    struct tm now_tm = *localtime( &now);
+    time_t futurNMois = now + 30*86400*nb_mois;
+    struct tm *nbMoisPlusTard_tm = localtime(&futurNMois);
+
+    day = nbMoisPlusTard_tm->tm_mday;
+    mois = nbMoisPlusTard_tm->tm_mon + 1;
+    an = nbMoisPlusTard_tm->tm_year + 1900;
+
+    Date * d = CreerDate(an,mois,day);
+
+    return d;
+}
