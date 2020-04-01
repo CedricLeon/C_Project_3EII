eb65c352677e4cc1728b61e3d711cbd188183b73
Magois Angeline <angeline.magois@insa-rennes.fr>
2020-04-01 21:22:26 +0200

    (fausse manip)

--- date.h
+++ date.h
@@ -1,64 +1,17 @@
-#include "date.h"
+#ifndef DATE_H
+#define DATE_H
 
-/**
- * CreerDate : Creer dynamiquement un objet Date
- * @param annee : l'annee de cette date
- * @param mois : le mois
- * @param jour : le jour
- * @return la date créée
- */
-Date * CreerDate(int annee, int mois, int jour){
-    Date * d = (Date *) malloc(sizeof(Date));
-    d->annee = annee;
-    d->mois = mois;
-    d->jour = jour;
-    return d;
-}
-/**
- * FreeDate : Libère la mémoire d'une instance Date
- * @param d
- */
-void FreeDate(Date * d){
-    free((void *) d);
-}
-/**
- * DateCourante : Créer une date correspondant à la date courante
- * @return la date courante
- */
-Date * DateCourante(){
-    int day, mois, an;
-    time_t now;
+#include <time.h>
 
-  time(&now);
+typedef struct{
+    int annee;
+    int mois;
+    int jour;
+}Date;
 
-  struct tm *local = localtime(&now);
-  day = local->tm_mday;
-  mois = local->tm_mon + 1;
-  an = local->tm_year + 1900;
+Date * CreerDate(int annee, int mois, int jour);
+Date * DateCourante();
+void FreeDate(Date * d);
+Date * AjoutMoisDateCourante(int nb_mois);
 
-  Date * d = CreerDate(an,mois,day);
-
-  return d;
-}
-
-/**
- * AjoutMoisDateCourante : Permet d'ajouter nb_mois mois à la date courante (utile notamment pour les ordonnances)
- * @param nb_mois
- * @return
- */
-Date * AjoutMoisDateCourante(int nb_mois){
-    int day, mois, an;
-    time_t now;
-    time(&now);
-    struct tm now_tm = *localtime( &now);
-    time_t futurNMois = now + 30*86400*nb_mois;
-    struct tm *nbMoisPlusTard_tm = localtime(&futurNMois);
-
-    day = nbMoisPlusTard_tm->tm_mday;
-    mois = nbMoisPlusTard_tm->tm_mon + 1;
-    an = nbMoisPlusTard_tm->tm_year + 1900;
-
-    Date * d = CreerDate(an,mois,day);
-
-    return d;
-}
+#endif
