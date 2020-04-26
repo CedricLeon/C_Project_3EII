9251304784e1f369821aabe3ae264173445148d7
Clouet Romane <romane.clouet@insa-rennes.fr>
2020-04-26 22:15:37 +0200

    Ajout fonctions cb_clicSurPlus et cb_recherchePatient

--- callbacks.c
+++ callbacks.c
@@ -39,3 +39,31 @@ void cb_create_entry1(GtkWidget *p_widget, gpointer user_data){
     gtk_box_pack_start(GTK_BOX(p_v_box), p_entry, TRUE, FALSE, 0);*/
     gtk_widget_show_all(p_window);
 }
+
+
+
+void cb_clicSurPlus(GtkWidget * widget, gpointer data){
+    fenetreRecherchePatient(widget, data);
+
+}
+
+void cb_recherchePatient(GtkWidget * widget, gpointer data){
+//    char * numPatient = malloc(14);
+//    char * numMedecin = malloc(12);
+//    //numPatient = data->                   // A FAIRE EN FONCTION DE L'IMPLEMENTATION DES MEDECINS DU PROJET
+//
+//    Medecin * m=NULL;
+//
+//    /* Parcours de la liste des patients de m */
+//    for(ListPatient_setOnFirst(m->patients_recus); ListPatient_isLast(m->patients_recus); ListPatient_setOnNext(m->patients_recus)){
+//        /* Si patient présent, on ouvre la fenêtre de création d'un RDV */
+//        if(strcmp(m->patients_recus->current->patient->numero_secu_social, numPatient)==0){
+//            fenetreCreerRDV();
+//        }else{
+//            fenetreCreerPatient();
+//        }
+//    }
+
+}
+
+
