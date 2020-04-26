c9ff339075e1788b71e8ce3441c5ff6528dad45b
Cabioch Elisabeth <elisabeth.cabioch@insa-rennes.fr>
2020-04-26 21:41:35 +0200

    time.h: calendar time gestion header

---
+++ time.h
@@ -0,0 +1,20 @@
+/*!
+* \file fonctionsGraphiques.h
+* \brief Provides basic unit testing functions declaration
+*/
+
+#ifndef TIME_H_INC
+#define TIME_H_INC
+
+#include <stdlib.h>
+#include <gtk/gtk.h>
+#include <stdio.h>
+#include <time.h>
+#include "fonctionsGraphiques.h"
+#include "callbacks.h"
+
+void create_calendar();
+
+//void JourCourantNom(char* result);
+
+#endif
