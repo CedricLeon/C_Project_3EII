86a39aea6cfc53a6e6b5799844d509267cc57cf5
Clouet Romane <romane.clouet@insa-rennes.fr>
2020-04-26 22:16:30 +0200

    ajout fonctions clicSurPlus et recherchePatient

--- callbacks.h
+++ callbacks.h
@@ -9,8 +9,11 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 #include "fonctionsGraphiques.h"
+#include "patient.h"
+#include "calendrier.h"
 
 void cb_create_entry(GtkWidget *p_widget, gpointer user_data);
 void cb_create_entry1(GtkWidget *p_widget, gpointer user_data);
-
+void cb_clicSurPlus(GtkWidget * widget, gpointer data);
+void cb_recherchePatient(GtkWidget * widget, gpointer data);
 #endif
