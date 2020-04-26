f02f86918cb2b80b6db38c21d50d620fbcff2451
Cedric Leonard <cedric.leonard@insa-rennes.fr>
2020-04-26 14:01:52 +0200

    Beginning to write cJSOn fonctiuns to save and load projects

--- src/JsonSave.c
+++ src/JsonSave.c
@@ -1,1 +1,76 @@
 #include "JsonSave.h"
+
+char* jsonSave(Project* p)
+{
+    //Création des variables qu'on va utiliser
+    char* string = NULL;
+    cJSON* listMedecinJson = NULL;
+    cJSON* listPatientJson = NULL;
+    cJSON* calendrierJson = NULL;
+
+    //On crée un objet cJSOn qu'on deletera après avoir print son contenu dans un string
+    cJSON* projectJson = cJSON_CreateObject();
+    if (cJSON_AddStringToObject(projectJson, "nom", p->nom) == NULL)  return NULL;
+
+    //On ajoute à notre objet cJSON un tableau appellé Working Medecins
+    listMedecinJson = cJSON_AddArrayToObject(projectJson, "Working Medecins");
+    if(listMedecinJson == NULL)                                             return NULL;
+    //On vient écrire dans cet objet tout les medecins
+    ListMedecin_jsonSave(listMedecinJson, p->workingMedecins);
+
+    //On ajoute à notre objet cJSON un tableau appellé Working Medecins
+    listPatientJson = cJSON_AddArrayToObject(projectJson, "Consulting Patients");
+    if(listPatientJson == NULL)                                             return NULL;
+    //On vient écrire dans cet objet tout les patients
+    ListPatient_jsonSave(listPatientJson, p->consultingPatient);
+
+    calendrierJson = cJSON_AddArrayToObject(projectJson, "Hospital Calendar");
+    if(calendrierJson == NULL)                                             return NULL;
+    Calendrier_jsonSave(calendrierJson, p->calendrier);
+
+    string = cJSON_Print(projectJson);
+    if (string == NULL)                                        fprintf(stderr, "Failed to print the project.\n");
+    cJSON_Delete(projectJson); //cette fonction appelle cJSON_Delete(listMedecinJson); et cJSON_Delete(listPatientJson);
+                               // puisque ces objets composent projectJson
+    return string;
+}
+
+
+
+//NOTE: Returns a heap allocated string, you are required to free it after use.
+char *create_monitor_with_helpers(void)
+{
+    const unsigned int resolution_numbers[3][2] = {
+            {1280, 720},
+            {1920, 1080},
+            {3840, 2160}
+    };
+    char *string = NULL;
+    cJSON *resolutions = NULL;
+    size_t index = 0;
+
+    cJSON *monitor = cJSON_CreateObject();
+    if (cJSON_AddStringToObject(monitor, "name", "Awesome 4K") == NULL)                           goto end;
+
+    resolutions = cJSON_AddArrayToObject(monitor, "resolutions");
+
+    if (resolutions == NULL)                                                                                   goto end;
+
+    for (index = 0; index < (sizeof(resolution_numbers) / (2 * sizeof(int))); ++index)
+    {
+        cJSON *resolution = cJSON_CreateObject();
+
+        if (cJSON_AddNumberToObject(resolution, "width", resolution_numbers[index][0]) == NULL)  goto end;
+
+        if (cJSON_AddNumberToObject(resolution, "height", resolution_numbers[index][1]) == NULL) goto end;
+
+        cJSON_AddItemToArray(resolutions, resolution);
+    }
+
+    string = cJSON_Print(monitor);
+    if (string == NULL)        fprintf(stderr, "Failed to print monitor.\n");
+
+    end:
+    cJSON_Delete(monitor);
+    return string;
+}
--- src/JsonSave.h
+++ src/JsonSave.h
@@ -16,6 +16,8 @@
 
 typedef struct
 {
+    //Nom du projet, pas vraiment d'utilité pour l'instant mais c'est pour comment marche cJSON
+    char* nom;
     //Liste de Medecins inscrits dans l'hopital
     ListMedecin * workingMedecins;
     //Liste des patients ayant déjà consulté dans cet hopital
@@ -23,35 +25,16 @@ typedef struct
     //Calendrier global de l'hopitail (pour la V0)
     Calendrier calendrier;
 }Project;
-/*
-char* jsonSave(Project*)
-{
-    Project p;
-    cJSON* projectJson = cJSON_CreateObject();
-    cJSON* listMedecinJson = cJSON_CreateObject();
-    cJSON* listPatientJson = cJSON_CreateObject();
-
-    MedecinList_jsonSave(listMedecinJson);
-    PatientList_jsonSave(listPatientJson);
 
 
-    string = cJSON_Print(monitor);
-    if (string == NULL)
-    {
-        fprintf(stderr, "Failed to print monitor.\n");
-    }
+void ListMedecin_jsonSave(cJSON* object, ListMedecin* l);
+void ListPatient_jsonSave(cJSON* object, ListPatient* l);
+void Calendrier_jsonSave(cJSON* object, Calendrier c);
 
-    //Cf example README
-    cJSON_Delete(projectJson);
-    cJSON_Delete(listMedecinJson, p->listMedecin); // ?
-    cJSON_Delete(listPatientJson);
-    return string;
-}
-bool jsonLoad(Project*, const char*);
+char* jsonSave(Project* p);
+int jsonLoad(Project*, const char*);
 
-MedecinList_jsonSave(cJSON* object, MedecinList* list);
-MedecinList_jsonLoad(cJSON* object);
-Medecin_jsonSave(cJSON* object);
-Medecin_jsonLoad(cJSON* object);*/
+
+
 
 #endif //C_PROJECT_JSONSAVE_H
