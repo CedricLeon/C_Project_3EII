8514ed3fd45e23509cb7f3a49399fcfe7a5c447c
Cedric Leonard <cedric.leonard@insa-rennes.fr>
2020-04-26 16:53:15 +0200

    Removing unused rues of FetchCJson.cmake

--- cmake/FetchCJson.cmake
+++ cmake/FetchCJson.cmake
@@ -10,8 +10,5 @@ FetchContent_Declare(
 
 set(ENABLE_CJSON_TEST OFF CACHE BOOL "CJson : Enable building the tests" FORCE)
 set(BUILD_SHARED_LIBS OFF CACHE BOOL "CJson : Build the shared libraries" FORCE)
-#set(WITH_EXAMPLES OFF CACHE BOOL "CMocka: Build the shared libraries" FORCE)
-#set(UNIT_TESTING OFF CACHE BOOL "CMocka: Build with unit testing" FORCE)
-#set(PICKY_DEVELOPER OFF CACHE BOOL "CMocka: Build with picky developer flags" FORCE)
 
 FetchContent_MakeAvailable(cjson)
