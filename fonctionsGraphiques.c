b98f3deef45efea5af959cf66cceddc2229622db
Clouet Romane <romane.clouet@insa-rennes.fr>
2020-04-28 22:45:33 +0200

    développement fenetreCreerPatient

--- fonctionsGraphiques.c
+++ fonctionsGraphiques.c
@@ -101,13 +101,13 @@ int create_window(int argc, char *argv[])
 
     /*Test d'ajout de Rendez-vous*/
 
-    GtkWidget * button1 = gtk_button_new_with_label("rdv1");
+    GtkWidget * button1 = gtk_button_new_with_label("fenetreCreerPatient");
     gtk_box_pack_start(GTK_BOX(boxLundi), button1, FALSE, FALSE, 0);
-    g_signal_connect(G_OBJECT(button1), "clicked", G_CALLBACK(cb_create_entry), NULL);
+    g_signal_connect(G_OBJECT(button1), "clicked", G_CALLBACK(fenetreCreerPatient), NULL);
 
-    GtkWidget * button2 = gtk_button_new_with_label("Rendez-vous n°2");
+    GtkWidget * button2 = gtk_button_new_with_label("fenetreCreerRDV");
     gtk_box_pack_start(GTK_BOX(boxJeudi), button2, FALSE, FALSE, 0);
-    g_signal_connect(G_OBJECT(button2), "clicked", G_CALLBACK(cb_create_entry), NULL);
+    g_signal_connect(G_OBJECT(button2), "clicked", G_CALLBACK(fenetreCreerRDV), NULL);
 
 
     gtk_widget_show_all(p_window);
@@ -132,7 +132,7 @@ void fenetreRecherchePatient(GtkWidget * widget, gpointer data)
 
     /* Widgets */
     GtkWidget * window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    GtkWidget * box0 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    GtkWidget * box0 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 20);
     GtkWidget * box1 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,0);
     GtkWidget * box2 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,0);
     GtkWidget * box3 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,0);
@@ -148,7 +148,7 @@ void fenetreRecherchePatient(GtkWidget * widget, gpointer data)
     gtk_entry_set_max_length(GTK_ENTRY(saisiePatient), 13);
     gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
     gtk_window_set_title(GTK_WINDOW(window), "Recherche du patient");
-    gtk_window_set_default_size(GTK_WINDOW(window), 700, 500);
+    gtk_window_set_default_size(GTK_WINDOW(window), 700, 400);
 
     char * tmp = malloc(50);
     getNomMedecin(tmp, m1);
@@ -196,10 +196,10 @@ void fenetreCreerRDV(GtkWidget * widget, gpointer data){
     GtkWidget * box6 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,0);
     GtkWidget * box7 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,0);
     GtkWidget * box8 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,50);
-    GtkWidget * box9 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,40);
+    GtkWidget * box9 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,10);
     GtkWidget * box10 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,0);
     GtkWidget * box11 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,0);
-    GtkWidget * labelHaut = gtk_label_new("Patient trouvé dans la base de données\n");
+    GtkWidget * labelHaut = gtk_label_new("Patient sélectionné dans la base de données\n");
     GtkWidget * labelNom = gtk_label_new("\tNom : ");
     GtkWidget * labelPrenom = gtk_label_new("\tPrénom : ");
     GtkWidget * labelDdN = gtk_label_new("\tDate de Naissance : ");
@@ -211,9 +211,16 @@ void fenetreCreerRDV(GtkWidget * widget, gpointer data){
     GtkWidget * choixMois = gtk_combo_box_text_new();
     GtkWidget * choixAnnee = gtk_combo_box_text_new();
     GtkWidget * choixHeure = gtk_combo_box_text_new();
+    GtkWidget * choixMinutes = gtk_combo_box_text_new();
     GtkWidget * choixDuree = gtk_combo_box_text_new();
     GtkWidget * labelHeure = gtk_label_new("\t Choix de l'heure : ");
-    GtkWidget * labelDuree = gtk_label_new("\t Choix de la durée : ");
+    GtkWidget * labelMinutes = gtk_label_new("    min : ");
+    GtkWidget * labelDuree = gtk_label_new("\t\t Durée du RDV : ");
+    GtkWidget * labelDuree2 = gtk_label_new("minutes");
+    GtkWidget * labelMotif = gtk_label_new("\t Motif du RDV : ");
+    GtkEntryBuffer * motif = gtk_entry_buffer_new(NULL, 300);
+    GtkWidget * saisirMotif = gtk_entry_new_with_buffer(motif);
+    GtkWidget * valider = gtk_button_new_with_label("VALIDER");
 
 //    char* nom = malloc(50);
 //    getNomPatient(nom, data);
@@ -233,17 +240,18 @@ void fenetreCreerRDV(GtkWidget * widget, gpointer data){
     gtk_widget_set_hexpand(ligne, TRUE);
     gtk_widget_set_halign(ligne, GTK_ALIGN_CENTER);
     gtk_label_set_justify(GTK_LABEL(ligne), GTK_JUSTIFY_CENTER);
+    gtk_widget_set_hexpand(valider, TRUE);
+    gtk_widget_set_halign(valider, GTK_ALIGN_CENTER);
 
 
 
-
-    /* Listes déroulantes  A FINIR */
+    /* Listes déroulantes */
         /* Liste jours */
-//    char j[0] = "";
-//    for(i=1; i<32; i++){
-//        j[0]=(char)i;
-//        gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixJour),j,j);
-//    }
+    char j[3];
+    for(i=1; i<32; i++){
+        sprintf(j,"%d",i);
+        gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixJour),j,j);
+    }
 
         /* Liste Mois */
     gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixMois), "1","Janvier");
@@ -260,10 +268,33 @@ void fenetreCreerRDV(GtkWidget * widget, gpointer data){
     gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixMois), "12","Décembre");
 
         /* Liste Années */
+    char a[5];
+    for(i=1930; i<2021; i++){
+        sprintf(a,"%d",i);
+        gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixAnnee),a,a);
+    }
+
+
         /* Liste Heures */
+    char h[3];
+    for(i=8; i<20; i++){
+        sprintf(h,"%d",i);
+        gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(choixHeure),h);
+    }
+
+        /* Liste Minutes */
+    char m[3];
+    for(i=0; i<=60; i=i+5){
+        sprintf(m,"%d",i);
+        gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(choixMinutes),m);
+    }
+
         /* Liste Durées */
-
-
+    char d[3];
+    for(i=0; i<=90; i=i+5){
+        sprintf(d,"%d",i);
+        gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(choixDuree),d);
+    }
 
     /* Ajout à la fenêtre */
     gtk_container_add (GTK_CONTAINER (window), box0);
@@ -291,8 +322,14 @@ void fenetreCreerRDV(GtkWidget * widget, gpointer data){
     gtk_container_add (GTK_CONTAINER (box8), choixAnnee);
     gtk_container_add (GTK_CONTAINER (box9), labelHeure);
     gtk_container_add (GTK_CONTAINER (box9), choixHeure);
+    gtk_container_add (GTK_CONTAINER (box9), labelMinutes);
+    gtk_container_add (GTK_CONTAINER (box9), choixMinutes);
     gtk_container_add (GTK_CONTAINER (box9), labelDuree);
     gtk_container_add (GTK_CONTAINER (box9), choixDuree);
+    gtk_container_add (GTK_CONTAINER (box9), labelDuree2);
+    gtk_container_add (GTK_CONTAINER (box10), labelMotif);
+    gtk_container_add (GTK_CONTAINER (box10), saisirMotif);
+    gtk_container_add (GTK_CONTAINER (box11), valider);
 
 
     gtk_widget_show_all(window);
@@ -300,11 +337,124 @@ void fenetreCreerRDV(GtkWidget * widget, gpointer data){
 
 }
 
-void fenetreCreerPatient(){
-    //A FAIRE
+void fenetreCreerPatient(GtkWidget * widget, gpointer data){
+
+    int i;
+
+    /* Widgets */
+        GtkWidget * window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    GtkWidget * box0 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 20);
+    GtkWidget * box1 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,20);
+    GtkWidget * box2 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,20);
+    GtkWidget * box3 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,20);
+    GtkWidget * box4 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,20);
+    GtkWidget * box5 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,20);
+    GtkWidget * box6 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,20);
+    GtkWidget * box7 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,20);
+    GtkWidget * box8 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,20);
+    GtkWidget * labelHaut = gtk_label_new("Entrez les informations du patient");
+    GtkWidget * labelNom = gtk_label_new("\tNom d'usage : ");
+    GtkWidget * labelPrenom = gtk_label_new("\tPrénom : ");
+    GtkWidget * labelDdN = gtk_label_new("\tDate de naissance : ");
+    GtkWidget * labelNumSS = gtk_label_new("\tNuméro de Sécurité Sociale : ");
+    GtkWidget * labelMail = gtk_label_new("\tAdresse e-mail : ");
+    GtkWidget * labelTel = gtk_label_new("\tNuméro de téléphone : ");
+    GtkEntryBuffer * nom = gtk_entry_buffer_new (NULL, 50);
+    GtkWidget * saisieNom = gtk_entry_new_with_buffer(GTK_ENTRY_BUFFER(nom));
+    GtkEntryBuffer * prenom = gtk_entry_buffer_new (NULL, 50);
+    GtkWidget * saisiePrenom = gtk_entry_new_with_buffer(GTK_ENTRY_BUFFER(prenom));
+    GtkEntryBuffer * numSecu = gtk_entry_buffer_new (NULL, 13);
+    GtkWidget * saisieNumSecu = gtk_entry_new_with_buffer(GTK_ENTRY_BUFFER(numSecu));
+    GtkEntryBuffer * mail = gtk_entry_buffer_new (NULL, 50);
+    GtkWidget * saisieMail = gtk_entry_new_with_buffer(GTK_ENTRY_BUFFER(mail));
+    GtkEntryBuffer * numTel = gtk_entry_buffer_new (NULL, 15);
+    GtkWidget * saisieTel = gtk_entry_new_with_buffer(GTK_ENTRY_BUFFER(numTel));
+    GtkWidget * choixJour = gtk_combo_box_text_new();
+    GtkWidget * choixMois = gtk_combo_box_text_new();
+    GtkWidget * choixAnnee = gtk_combo_box_text_new();
+    GtkWidget * labelBarre1 = gtk_label_new("/");
+    GtkWidget * labelBarre2 = gtk_label_new("/");
+    GtkWidget * valider = gtk_button_new_with_label("VALIDER");
 
 
 
+    /* Listes déroulantes */
+        /* Liste jours */
+    char j[3];
+    for(i=1; i<32; i++){
+        sprintf(j,"%d",i);
+        gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixJour),j,j);
+    }
+
+
+        /* Liste Mois */
+    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixMois), "1","Janvier");
+    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixMois), "2","Février");
+    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixMois), "3","Mars");
+    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixMois), "4","Avril");
+    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixMois), "5","Mai");
+    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixMois), "6","Juin");
+    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixMois), "7","Juillet");
+    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixMois), "8","Août");
+    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixMois), "9","Septembre");
+    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixMois), "10","Octobre");
+    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixMois), "11","Novembre");
+    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixMois), "12","Décembre");
+
+
+        /* Liste Années */
+    char a[5];
+    for(i=1900; i<2021; i++){
+        sprintf(a,"%d",i);
+        gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(choixAnnee),a,a);
+    }
+
+
+    /* Paramètres graphiques des widgets */
+    gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
+    gtk_window_set_title(GTK_WINDOW(window), "Création d'un patient");
+    gtk_window_set_default_size(GTK_WINDOW(window), 700, 500);
+    gtk_widget_set_hexpand(labelHaut, TRUE);
+    gtk_widget_set_halign(labelHaut, GTK_ALIGN_CENTER);
+    gtk_label_set_justify(GTK_LABEL(labelHaut), GTK_JUSTIFY_CENTER);
+    gtk_widget_set_hexpand(valider, TRUE);
+    gtk_widget_set_halign(valider, GTK_ALIGN_CENTER);
+
+        /* Ajout à la fenêtre */
+    gtk_container_add (GTK_CONTAINER (window), box0);
+    gtk_container_add (GTK_CONTAINER (box0), box1);
+    gtk_container_add (GTK_CONTAINER (box0), box2);
+    gtk_container_add (GTK_CONTAINER (box0), box3);
+    gtk_container_add (GTK_CONTAINER (box0), box4);
+    gtk_container_add (GTK_CONTAINER (box0), box5);
+    gtk_container_add (GTK_CONTAINER (box0), box6);
+    gtk_container_add (GTK_CONTAINER (box0), box7);
+    gtk_container_add (GTK_CONTAINER (box0), box8);
+    gtk_container_add (GTK_CONTAINER (box1), labelHaut);
+    gtk_container_add (GTK_CONTAINER (box2), labelNom);
+    gtk_container_add (GTK_CONTAINER (box3), labelPrenom);
+    gtk_container_add (GTK_CONTAINER (box4), labelDdN);
+    gtk_container_add (GTK_CONTAINER (box5), labelNumSS);
+    gtk_container_add (GTK_CONTAINER (box6), labelMail);
+    gtk_container_add (GTK_CONTAINER (box7), labelTel);
+    gtk_container_add (GTK_CONTAINER (box2), saisieNom);
+    gtk_container_add (GTK_CONTAINER (box3), saisiePrenom);
+    gtk_container_add (GTK_CONTAINER (box5), saisieNumSecu);
+    gtk_container_add (GTK_CONTAINER (box6), saisieMail);
+    gtk_container_add (GTK_CONTAINER (box7), saisieTel);
+    gtk_container_add (GTK_CONTAINER (box4), choixJour);
+    gtk_container_add (GTK_CONTAINER (box4), labelBarre1);
+    gtk_container_add (GTK_CONTAINER (box4), choixMois);
+    gtk_container_add (GTK_CONTAINER (box4), labelBarre2);
+    gtk_container_add (GTK_CONTAINER (box4), choixAnnee);
+    gtk_container_add (GTK_CONTAINER (box8), valider);
+
+    gtk_widget_show_all(window);
+
+
+    /* callbacks */
+
+     g_signal_connect(G_OBJECT(valider), "clicked", G_CALLBACK(cb_creationPatient), data);
 
 }
 
