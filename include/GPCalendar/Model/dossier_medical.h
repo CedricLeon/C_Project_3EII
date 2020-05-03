901f98c2487920e9216e243072a36495452cd1e1
Magois Angeline <angeline.magois@insa-rennes.fr>
2020-05-03 22:54:12 +0200

    Ajout d'une ListAntecedent dans DossierMedical et ecriture de la fonction AddAntecedent

--- include/GPCalendar/Model/dossier_medical.h
+++ include/GPCalendar/Model/dossier_medical.h
@@ -8,26 +8,62 @@
 #include <stdio.h>
 
 
+/**********************************************************************************************************************/
+                                                /*ListAntecedent*/
+/**********************************************************************************************************************/
+
+/**
+ * Structure NodeAntecedent
+ */
+typedef struct NodeAntecedent NodeAntecedent;
+struct NodeAntecedent{
+    char * ante;
+    NodeAntecedent * previous;
+    NodeAntecedent * next;
+};
+
+typedef struct{
+    NodeAntecedent sentinel_begin;
+    NodeAntecedent * current;
+    NodeAntecedent sentinel_end;
+}ListAntecedent;
+
+NodeAntecedent * newNodeAntecedent(char * ante, NodeAntecedent * previous, NodeAntecedent * next);
+void freeNodeAntecedent(ListAntecedent *l, NodeAntecedent * n);
+
+void ListAntecedent_init(ListAntecedent * l);
+void ListAntecedent_free(ListAntecedent * l);
+
+int ListAntecedent_isEmpty(ListAntecedent * l);
+int ListAntecedent_isFirst(ListAntecedent * l);
+int ListAntecedent_isLast(ListAntecedent * l);
+int ListAntecedent_isOutOfList(ListAntecedent * l);
+
+void ListAntecedent_setOnFirst(ListAntecedent * l);
+void ListAntecedent_setOnLast(ListAntecedent * l);
+void ListAntecedent_setOnNext(ListAntecedent * l);
+void ListAntecedent_setOnPrevious(ListAntecedent * l);
+char * ListAntecedent_getCurrent(ListAntecedent * l);
+
+/**********************************************************************************************************************/
+                                                /*DossierMedical*/
+/**********************************************************************************************************************/
+
+
 typedef struct DossierMedical{
     ListMedecin* medecins_consultes; //Liste des m�decins consult�s pour ce dossier
     ListOrdonnance* ordonnances;
     /**
      * Pas d'antécédents pour l'instant (pas implémenté dans dossier médical.c et dans Json.c
      */
-   // char antecedents[]; //CR des pr�c�dents passage � l'hopital pour les prochaines versions
+    ListAntecedent* antecedents;//CR des précédents passages à l'hopital pour les prochaines versions
 }DossierMedical;
 
 DossierMedical * CreerDossierMedical();
 void FreeDossierMedical(DossierMedical * d);
-void AccesDossier(Patient * p);    //Print toutes les infos je suppose
-
-//void SetPatientDossier(DossierMedical * dm, Patient * patient); -> plus besoin comme le dossier fait partie d'un patient ?
-// int AddMedecinDossierMedical(DossierMedical * dm, Medecin * medecin); ->plus besoin du coup fonction déjà présente dans patient.c
-
+void AccesDossier(Patient * p);
 
 int AddOrdonnanceDossierMedical(DossierMedical * dm, Ordonnance * ordonnance);
-int AddAntecedentDossierMedical(DossierMedical * dm, char antecedent);
-
-//void TransfertDossier(DossierMedical dm, ???); //J'avoue que je vois pas trop
+int AddAntecedentDossierMedical(DossierMedical * dm);
 
 #endif
--- src/Model/dossier_medical.c
+++ src/Model/dossier_medical.c
@@ -14,21 +14,24 @@ DossierMedical * CreerDossierMedical(){
     dm->ordonnances = (ListOrdonnance*) malloc(sizeof(ListOrdonnance));
     ListOrdonnance_init(dm->ordonnances);
 
-    //On initialise pas les antécédents
+    dm->antecedents = (ListAntecedent*) malloc(sizeof(ListAntecedent));
+    ListAntecedent_init(dm->antecedents);
 
     return dm;
 }
 
 /**
  * FreeDossier : Free un objet DossierMedical
- * @param dm : le dossier � supprimer
+ * @param dm : le dossier à supprimer
  */
 void FreeDossierMedical(DossierMedical * dm){
     printf("FreeDossierMedical() : \n");
     ListOrdonnance_free(dm->ordonnances);     // Il faut delete les ordonnances
     ListMedecin_free_withoutDeletingMedecin(dm->medecins_consultes);    // Il ne faut pas delete les mèdecins référencés
+
                                                                         // par cette liste puisqu'on les delete dèjà
-                                                                        // depuis la liste workingMedecins
+    ListAntecedent_free(dm->antecedents);                                                                   // depuis la liste workingMedecins
+
     free((void *) dm);
 }
 
@@ -63,5 +66,198 @@ int AddOrdonnanceDossierMedical(DossierMedical * dm, Ordonnance * ordonnance){
  * @param antecedent : l'antecedent à ajouter
  */
 
-//int AddAntecedentDossierMedical(DossierMedical * dm, char antecedent){}
+int AddAntecedentDossierMedical(DossierMedical * dm){
+    char * ante;
+    char  * ant;
+     if(dm == NULL){
+        printf("DossierMedical NULL.\n");
+        return -1;
+    }
+    //Si Liste vide on insère au début
+    if(ListAntecedent_isEmpty(dm->antecedents)){
+        scanf("Compte-rendu du rdv : %s", ante);
+        NodeAntecedent * newNode = newNodeAntecedent(ante, &(dm->antecedents->sentinel_begin), &(dm->antecedents->sentinel_end));
+        dm->antecedents->sentinel_begin.next = newNode;
+        dm->antecedents->sentinel_end.previous = newNode;
+        return 1;
+    }
+    //Si pas vide on insère l'ordonnance à la fin
+    scanf("Compte-rendu du rdv : %s", ante);
+    NodeAntecedent * newNode = newNodeAntecedent(ant, dm->antecedents->sentinel_end.previous, &(dm->antecedents->sentinel_end));
+    dm->antecedents->sentinel_end.previous->next = newNode;
+    dm->antecedents->sentinel_end.previous = newNode;
+    return 1;
+}
+
+
+
+
+/**********************************************************************************************************************/
+                                                /*ListAntecedent*/
+/**********************************************************************************************************************/
+
+/**
+ * newNodeAntecedent : Permet de créer dynamiquement un nouveau node de Antecedent pour la liste
+ * @param Antecedent : l'antecedent pointé par ce nouveau noeud
+ * @param previous : le noeud précédant le nouveau noeud dans la liste
+ * @param next : le prochain noeud de la liste
+ * @return un pointeur sur le nouveau noeud créé
+ */
+NodeAntecedent * newNodeAntecedent(char * ante, NodeAntecedent * previous, NodeAntecedent * next){
+    NodeAntecedent * newNode = (NodeAntecedent *) malloc(sizeof(NodeAntecedent));
+    newNode->ante = ante;
+    newNode->next = next;
+    newNode->previous = previous;
+    return newNode;
+}
+/**
+ * freeNodeAntecedent : Permet de delete proprement (avec un free) un NodeAntecedent
+ * @param n : le node à delete
+ */
+void freeNodeAntecedent(ListAntecedent *l, NodeAntecedent * n){
+    n->previous->next = n->next;
+    n->next->previous = n->previous;
+    free((void *) n);
+    ListAntecedent_setOnFirst(l);
+    ListAntecedent_setOnPrevious(l);
+}
+
+
+/**
+ * ListAntecedent_init : Initialise correctement une liste de NodeAntecedent en reliant sentinel_begin et end entre eux
+ * et en mettant current à NULL (en dehors de la liste)
+ * @param l : la liste à initialiser
+ */
+void ListAntecedent_init(ListAntecedent * l){
+    if (l != NULL){
+        l->current = NULL;
+        l->sentinel_begin.next = &(l->sentinel_end);
+        l->sentinel_begin.previous = NULL;
+        l->sentinel_begin.ante = NULL;
+        l->sentinel_end.previous = &(l->sentinel_begin);
+        l->sentinel_end.next = NULL;
+        l->sentinel_end.ante = NULL;
+    }
+}
+/**
+ * ListAntecedent_free : Libère la mémoire occupée par l'objet ListAntecedent passé en paramètre
+ * @param l : la liste de antecedents à free
+ */
+void ListAntecedent_free(ListAntecedent * l){
+    if (l == NULL){
+        printf("ListAntecedent_free : la liste d'antecedents est NULL !!!\n");
+    }else if (ListAntecedent_isEmpty(l)){
+        printf("ListAntecedent_free : la liste est vide, on free donc uniquement la liste\n");
+        free((void *) l);
+    }else{
+        for(ListAntecedent_setOnFirst(l); !ListAntecedent_isOutOfList(l); ListAntecedent_setOnNext(l)) {
+            printf("ListAntecedent_free : Appel de freeNodeAntecedent() pour l'antecedent : %s\n", l->current->ante);
+            freeNodeAntecedent(l, l->current);
+        }
+        free((void *) l);
+    }
+}
+
+
+/**
+ * ListAntecedent_isEmpty : Vérifie si la liste de Antecedent est vide ou non
+ * @param l : la liste
+ * @return 1 si la liste est vide
+ *         0 si elle ne l'est pas
+ *         -1 si la liste est NULL
+ */
+int ListAntecedent_isEmpty(ListAntecedent * l){
+    if (l != NULL){
+        return  (l->sentinel_begin.next == &(l->sentinel_end)) && (l->sentinel_end.previous == &(l->sentinel_begin));
+    }
+    return -1; //La liste est NULL
+}
+/**
+ * ListAntecedent_isFirst : Vérifie si current est positionné sur le premier élément de la liste
+ * @param l : la liste
+ * @return 1 si current est bien sur le premier élément
+ *         0 si il ne l'est pas
+ *         -1 si la liste est NULL
+ */
+int ListAntecedent_isFirst(ListAntecedent * l){
+    if (l != NULL){
+        return  l->current == l->sentinel_begin.next;
+    }
+    return -1; //La liste est NULL
+}
+/**
+ * ListAntecedent_isLast : Vérifie si current est positionné sur le dernier élément de la liste
+ * @param l : la liste
+ * @return 1 si current est bien sur le dernier élément
+ *         0 si il ne l'est pas
+ *         -1 si la liste est NULL
+ */
+int ListAntecedent_isLast(ListAntecedent * l){
+    if (l != NULL){
+        return  l->current == l->sentinel_end.previous;
+    }
+    return -1; //La liste est NULL
+}
+/**
+ * ListAntecedent_isOutOfList : Vérifie si current est bien placé sur un élément de la liste
+ * (les sentinels ne sont pas considérées comme dans la liste)
+ * @param l : la liste
+ * @return 1 si current vaut NULL
+ *         0 sinon
+ *         -1 si la liste est NULL
+ */
+int ListAntecedent_isOutOfList(ListAntecedent * l){
+    if (l != NULL){
+        return  (l->current == NULL) || (l->current == &(l->sentinel_begin)) || (l->current == &(l->sentinel_end));
+    }
+    return -1; //La liste est NULL
+}
+
+/**
+ * ListAntecedent_setOnFirst : Positionne le pointeur courant sur le premier élément de la liste
+ * @param l : la liste
+ */
+void ListAntecedent_setOnFirst(ListAntecedent * l){
+    if(l != NULL){
+        l->current = l->sentinel_begin.next;
+    }
+}
+/**
+ * ListAntecedent_setOnLast : Positionne le pointeur courant sur le dernier élément de la liste
+ * @param l : la liste
+ */
+void ListAntecedent_setOnLast(ListAntecedent * l){
+    if(l != NULL){
+        l->current = l->sentinel_end.previous;
+    }
+}
+/**
+ * ListAntecedent_setOnNext : Positionne le pointeur courant sur le prochain élément de la liste
+ * @param l : la liste
+ */
+void ListAntecedent_setOnNext(ListAntecedent * l){
+    if(l != NULL && l->current->next != NULL){
+        l->current = l->current->next;
+    }
+}
+/**
+ * ListAntecedent_setOnPrevious : Positionne le pointeur courant sur l'élément le précédant dans la liste
+ * @param l : la liste
+ */
+void ListAntecedent_setOnPrevious(ListAntecedent * l){
+    if(l != NULL && l->current->previous != NULL){
+        l->current = l->current->previous;
+    }
+}
+/**
+ * ListAntecedent_getCurrent : Permet d'acceder à l'Ordonnance pointée par current
+ * @param l : la liste
+ * @return Retourne un pointeur sur l'Ordonnance de l'élément courant de la liste
+ */
+char * ListAntecedent_getCurrent(ListAntecedent * l){
+    if(l != NULL && l->current != NULL){
+        return l->current->ante;
+    }
+    return NULL;
+}
 
