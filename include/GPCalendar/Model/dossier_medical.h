43259d6345eba2b969aa4291fd4048c37e4dc9cb
Cedric Leonard <cedric.leonard@insa-rennes.fr>
2020-05-05 10:43:34 +0200

    Cleaning declaration of DossierMedical and ListAntecedents (after reverting this content from a previous commit)

--- include/GPCalendar/Model/dossier_medical.h
+++ include/GPCalendar/Model/dossier_medical.h
@@ -7,48 +7,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-/**********************************************************************************************************************/
-                                                /*ListAntecedent*/
-/**********************************************************************************************************************/
-
-/**
- * Structure NodeAntecedent
- */
-typedef struct NodeAntecedent NodeAntecedent;
-struct NodeAntecedent{
-    char * ante;
-    NodeAntecedent * previous;
-    NodeAntecedent * next;
-};
-
-typedef struct{
-    NodeAntecedent sentinel_begin;
-    NodeAntecedent * current;
-    NodeAntecedent sentinel_end;
-}ListAntecedent;
-
-NodeAntecedent * newNodeAntecedent(char * ante, NodeAntecedent * previous, NodeAntecedent * next);
-void freeNodeAntecedent(ListAntecedent *l, NodeAntecedent * n);
-
-void ListAntecedent_init(ListAntecedent * l);
-void ListAntecedent_free(ListAntecedent * l);
-
-int ListAntecedent_isEmpty(ListAntecedent * l);
-int ListAntecedent_isFirst(ListAntecedent * l);
-int ListAntecedent_isLast(ListAntecedent * l);
-int ListAntecedent_isOutOfList(ListAntecedent * l);
-
-void ListAntecedent_setOnFirst(ListAntecedent * l);
-void ListAntecedent_setOnLast(ListAntecedent * l);
-void ListAntecedent_setOnNext(ListAntecedent * l);
-void ListAntecedent_setOnPrevious(ListAntecedent * l);
-char * ListAntecedent_getCurrent(ListAntecedent * l);
-
 /**********************************************************************************************************************/
                                                 /*DossierMedical*/
 /**********************************************************************************************************************/
 
+typedef struct ListAntecedent ListAntecedent;
 
 typedef struct DossierMedical{
     ListMedecin* medecins_consultes; //Liste des m�decins consult�s pour ce dossier
@@ -66,5 +29,42 @@ void AccesDossier(Patient * p);
 int AddOrdonnanceDossierMedical(DossierMedical * dm, Ordonnance * ordonnance);
 int AddAntecedentDossierMedical(DossierMedical * dm, char * ant);
 void printAntecedent(char * ante);
+
+/**********************************************************************************************************************/
+                                                /*ListAntecedent*/
+/**********************************************************************************************************************/
+
+/**
+ * Structure NodeAntecedent
+ */
+typedef struct NodeAntecedent NodeAntecedent;
+struct NodeAntecedent{
+    char * ante;
+    NodeAntecedent * previous;
+    NodeAntecedent * next;
+};
+
+struct ListAntecedent{
+    NodeAntecedent sentinel_begin;
+    NodeAntecedent * current;
+    NodeAntecedent sentinel_end;
+};
+
+NodeAntecedent * newNodeAntecedent(char * ante, NodeAntecedent * previous, NodeAntecedent * next);
+void freeNodeAntecedent(ListAntecedent *l, NodeAntecedent * n);
+
+void ListAntecedent_init(ListAntecedent * l);
+void ListAntecedent_free(ListAntecedent * l);
+
+int ListAntecedent_isEmpty(ListAntecedent * l);
+int ListAntecedent_isFirst(ListAntecedent * l);
+int ListAntecedent_isLast(ListAntecedent * l);
+int ListAntecedent_isOutOfList(ListAntecedent * l);
+
+void ListAntecedent_setOnFirst(ListAntecedent * l);
+void ListAntecedent_setOnLast(ListAntecedent * l);
+void ListAntecedent_setOnNext(ListAntecedent * l);
+void ListAntecedent_setOnPrevious(ListAntecedent * l);
+char * ListAntecedent_getCurrent(ListAntecedent * l);
 
 #endif
