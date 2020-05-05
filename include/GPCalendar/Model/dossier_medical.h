60422ab99c866ca11940d8af3f854f949f48a96f
Cedric Leonard <cedric.leonard@insa-rennes.fr>
2020-05-05 10:25:15 +0200

    Cleaning declaration of DossierMedical and ListAntecedents (+commenting its tests en tests_unitaires.c because of infinite loop)

--- include/GPCalendar/Model/dossier_medical.h
+++ include/GPCalendar/Model/dossier_medical.h
@@ -7,6 +7,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**********************************************************************************************************************/
+                                                /*DossierMedical*/
+/**********************************************************************************************************************/
+
+typedef struct ListAntecedent ListAntecedent;
+
+typedef struct DossierMedical{
+    ListMedecin* medecins_consultes; //Liste des m�decins consult�s pour ce dossier
+    ListOrdonnance* ordonnances;
+    /**
+     * Pas d'antécédents pour l'instant (pas implémenté dans dossier médical.c et dans Json.c
+     */
+    ListAntecedent* antecedents;  //CR des précédents passages à l'hopital pour les prochaines versions
+}DossierMedical;
+
+DossierMedical * CreerDossierMedical();
+void FreeDossierMedical(DossierMedical * d);
+void AccesDossier(Patient * p);
+
+int AddOrdonnanceDossierMedical(DossierMedical * dm, Ordonnance * ordonnance);
+int AddAntecedentDossierMedical(DossierMedical * dm, char * ant);
+void printAntecedent(char * ante);
 
 /**********************************************************************************************************************/
                                                 /*ListAntecedent*/
@@ -22,11 +44,11 @@ struct NodeAntecedent{
     NodeAntecedent * next;
 };
 
-typedef struct{
+struct ListAntecedent{
     NodeAntecedent sentinel_begin;
     NodeAntecedent * current;
     NodeAntecedent sentinel_end;
-}ListAntecedent;
+};
 
 NodeAntecedent * newNodeAntecedent(char * ante, NodeAntecedent * previous, NodeAntecedent * next);
 void freeNodeAntecedent(ListAntecedent *l, NodeAntecedent * n);
@@ -44,27 +66,5 @@ void ListAntecedent_setOnLast(ListAntecedent * l);
 void ListAntecedent_setOnNext(ListAntecedent * l);
 void ListAntecedent_setOnPrevious(ListAntecedent * l);
 char * ListAntecedent_getCurrent(ListAntecedent * l);
-
-/**********************************************************************************************************************/
-                                                /*DossierMedical*/
-/**********************************************************************************************************************/
-
-
-typedef struct DossierMedical{
-    ListMedecin* medecins_consultes; //Liste des m�decins consult�s pour ce dossier
-    ListOrdonnance* ordonnances;
-    /**
-     * Pas d'antécédents pour l'instant (pas implémenté dans dossier médical.c et dans Json.c
-     */
-    ListAntecedent* antecedents;//CR des précédents passages à l'hopital pour les prochaines versions
-}DossierMedical;
-
-DossierMedical * CreerDossierMedical();
-void FreeDossierMedical(DossierMedical * d);
-void AccesDossier(Patient * p);
-
-int AddOrdonnanceDossierMedical(DossierMedical * dm, Ordonnance * ordonnance);
-int AddAntecedentDossierMedical(DossierMedical * dm, char * ant);
-void printAntecedent(char * ante);
 
 #endif
